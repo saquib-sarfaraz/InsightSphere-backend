d1315cea941425f20462d4b448fa95c27924357e
saquib-sarfaraz <saquibsarfaraz47@gmail.com>
2026-05-18 15:16:46 +0530

    update

--- server/src/app.js
+++ server/src/app.js
@@ -22,8 +22,10 @@ app.use(
   cors({
     origin: env.CORS_ORIGIN,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   }),
 )
+app.options('*', cors())
 app.use(compression())
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({ extended: true }))
