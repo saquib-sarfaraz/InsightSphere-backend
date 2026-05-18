df48b1e6d9e4f7d78cb971e1e228fb476b1c6ae1
saquib-sarfaraz <saquibsarfaraz47@gmail.com>
2026-05-18 15:24:14 +0530

    Fix express cors path wildcard crash

--- server/src/app.js
+++ server/src/app.js
@@ -25,7 +25,6 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   }),
 )
-app.options('*', cors())
 app.use(compression())
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({ extended: true }))
